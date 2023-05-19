373dd60784501aa3a8a30038830998504296f4f5
Miguel Porfírio <miguelporfirio64@gmail.com>
2023-05-19 23:41:12 +0100

    More progress on inventory and warning fixed :smile:

--- Assets/Scripts/UI/UIInventoryPage.cs
+++ Assets/Scripts/UI/UIInventoryPage.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -17,9 +18,39 @@ public class UIInventoryPage : MonoBehaviour
         for (int i = 0; i < inventorysize; i++)
         {
             UIInventoryItem uiItem = Instantiate(itemPrefab, Vector3.zero, Quaternion.identity);
-            uiItem.transform.parent = contentPanel;
+            uiItem.transform.SetParent(contentPanel, true);
             listOfUIItems.Add(uiItem);
+            uiItem.OnItemClicked += HandleItemSelection;
+            uiItem.OnItemBeginDrag += HandleBeginDrag;
+            uiItem.OnItemDroppedOn += HandleSwap;
+            uiItem.OnItemEndDrag += HandleEndDrag;
+            uiItem.OnRightMouseBtnClick += HandleShowItemActions;
         }
+    }
+
+    private void HandleShowItemActions(UIInventoryItem obj)
+    {
+
+    }
+
+    private void HandleEndDrag(UIInventoryItem obj)
+    {
+
+    }
+
+    private void HandleSwap(UIInventoryItem obj)
+    {
+
+    }
+
+    private void HandleBeginDrag(UIInventoryItem obj)
+    {
+
+    }
+
+    private void HandleItemSelection(UIInventoryItem obj)
+    {
+
     }
 
     public void Show()
