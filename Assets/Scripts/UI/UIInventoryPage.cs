4d517e30ff86c167499ed87643ffba479ca39329
Miguel Porfírio <miguelporfirio64@gmail.com>
2023-05-19 04:31:21 +0100

    First half of inventory and item interaction implementation

---
+++ Assets/Scripts/UI/UIInventoryPage.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIInventoryPage : MonoBehaviour
+{
+    [SerializeField]
+    private UIInventoryItem itemPrefab;
+
+    [SerializeField]
+    private RectTransform contentPanel;
+
+    List<UIInventoryItem> listOfUIItems = new List<UIInventoryItem>();
+
+    public void InitializeInventoryUI(int inventorysize)
+    {
+        for (int i = 0; i < inventorysize; i++)
+        {
+            UIInventoryItem uiItem = Instantiate(itemPrefab, Vector3.zero, Quaternion.identity);
+            uiItem.transform.parent = contentPanel;
+            listOfUIItems.Add(uiItem);
+        }
+    }
+
+    public void Show()
+    {
+        gameObject.SetActive(true);
+    }
+
+    public void Hide()
+    {
+        gameObject.SetActive(false);
+    }
+}
