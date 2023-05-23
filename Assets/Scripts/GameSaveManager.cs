7824abf0f036e312aa4748dc66de87825f5d9876
SaraBuu <sara.aguincha@gmail.com>
2023-05-23 16:20:20 +0100

    MainMenu and PauseMenu added

--- Assets/Scripts/GameSaveManager.cs
+++ Assets/Scripts/GameSaveManager.cs
@@ -1,5 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
 using UnityEngine;
 
 public class GameSaveManager : MonoBehaviour
@@ -19,4 +21,59 @@ public class GameSaveManager : MonoBehaviour
         }
         DontDestroyOnLoad(this);
     }
+
+    private void OnEnable()
+    {
+        LoadObjects();
+        Debug.Log(Application.persistentDataPath);
+    }
+
+    private void OnDisable()
+    {
+        SaveObjects();
+    }
+
+
+    public void SaveObjects()
+    {
+        for (int i = 0; i < objects.Count; i++)
+        {
+            FileStream file = File.Create(Application.persistentDataPath +
+                string.Format("/{0}.dat", i));
+            BinaryFormatter binary = new BinaryFormatter();
+            var json = JsonUtility.ToJson(objects[i]);
+            binary.Serialize(file, json);
+            file.Close();
+        }
+    }
+
+    public void LoadObjects()
+    {
+        for (int i = 0; i < objects.Count; i++)
+        {
+            if (File.Exists(Application.persistentDataPath +
+                string.Format("/{0}.dat", i)))
+            {
+                FileStream file = File.Open(Application.persistentDataPath +
+                    string.Format("/{0}.dat", i), FileMode.Open);
+                BinaryFormatter binary = new BinaryFormatter();
+                JsonUtility.FromJsonOverwrite((string)binary.Deserialize(file), objects[i]);
+                file.Close();
+            }
+        }
+    }
+
+    public void ResetObjects()
+    {
+        for (int i = 0; i < objects.Count; i++)
+        {
+            if (File.Exists(Application.persistentDataPath +
+                string.Format("/{0}.dat", i)))
+            {
+                File.Delete(Application.persistentDataPath +
+                string.Format("/{0}.dat", i));
+            }
+        }
+    }
+
 }
